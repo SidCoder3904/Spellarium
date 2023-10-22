db0787d606d998c8d15e0c082277f7c474dd1c33
Siddharth Verma <siddharthverma3904@gmail.com>
2023-10-23 01:47:37 +0530

    added trie and levenshtein distance algo

--- spellChecker.c
+++ spellChecker.c
@@ -1,19 +1,28 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <stdbool.h>
-# include <stdint.h>
-# include <string.h>
-# include <ctype.h>
+# include <stdio.h>     // standard C library
+# include <stdlib.h>    // for dynamic memory allocation
+# include <stdbool.h>   // for bool datatype
+# include <stdint.h>    // for uint32 types etc
+# include <string.h>    // for string manipulation
+# include <limits.h>    // for INT_MAX, max min values
+# include <ctype.h>     // for formatting
 
-#define filterSize 10000000
-#define k 8
+// Bloom Filter variables
+#define FILTER_SIZE 10000000
+#define K 8     // no. of hash functions
+
+// Trie variables
+#define MAX_LENGTH 50
+#define MAX_SUGGESTIONS 100
+#define MAX_INCRT_WORDS 25
+#define N 26    // no. of distinct characters in language
+
 
 // hash functions
 uint32_t djb2(const char* string) {
     uint32_t hash = 5381;
     int c;
     while ((c= *string++)) hash = (hash << 5) + hash + c;    // hash*33 + c
-    return hash % filterSize;
+    return hash % FILTER_SIZE;
 }
 
 uint32_t jenkin(const char* str) {
@@ -26,33 +35,103 @@ uint32_t jenkin(const char* str) {
     hash += (hash << 3);
     hash ^= (hash >> 11);
     hash += (hash << 15);
-    return hash % filterSize;
+    return hash % FILTER_SIZE;
 }
 
-bool* filter;
-
-void insert(char* string) {    // bloom filter insertion
-    for(int i=0; i<k; i++) filter[(djb2(string)*i + jenkin(string))%filterSize] = 1;
+void insertFilter(bool* filter, char* word) {    // bloom filter insertion
+    for(int i=0; i<K; i++) filter[(djb2(word)*i + jenkin(word))%FILTER_SIZE] = 1;
 }
 
-int search(char* string) {      // searching in bloom filter
-    for(int i=0; i<k; i++) if(!filter[(djb2(string)*i + jenkin(string))%filterSize]) return 0;
+int searchFilter(bool* filter, char* word) {      // searching in bloom filter
+    for(int i=0; i<K; i++) if(!filter[(djb2(word)*i + jenkin(word))%FILTER_SIZE]) return 0;
     return 1;
 }
 
-void loadDictionary(){
-    FILE *file = fopen("dictionary.csv", "r");
-    char string[100];
-    while (fscanf(file, "%s", string) != EOF) insert(string);
+// Trie functions
+int smallest(int a, int b, int c) {      // helper function returns min(a, b, c)
+    return a < b ? (a < c ? a : c) : (b < c ? b : c);
+}
+
+typedef struct node {   // trie node
+    struct node *child[26];
+    int isEOW;
+} TRIE_NODE;
+
+TRIE_NODE* createNode() {
+    TRIE_NODE* pNode = malloc(sizeof(TRIE_NODE));
+    pNode->isEOW = 0;
+    for(int i=0; i<26; i++) pNode->child[i] = NULL;
+    return pNode;
+}
+
+void insertTrie(TRIE_NODE* root, char* word){
+    TRIE_NODE* pCrawl = root;
+    int len = strlen(word), idx;
+    for(int i=0; i<len; i++) {
+        idx = word[i] - 'a';
+        if(!pCrawl->child[idx]) pCrawl->child[idx] = createNode();
+        pCrawl = pCrawl->child[idx];
+    }
+    pCrawl->isEOW = 1;
+}
+
+int searchTrie(TRIE_NODE* root, char* word){
+    TRIE_NODE* pCrawl = root;
+    int len = strlen(word), idx;
+    for(int i=0; i<len; i++) {
+        idx = word[i] - 'a';
+        if(!pCrawl->child[idx]) return 0;
+        pCrawl = pCrawl->child[idx];
+    }
+    return (pCrawl->isEOW);
+}
+
+void display(TRIE_NODE* root, char* word, int level){
+    if(root->isEOW) word[level] = '\0';
+    for(int i=0; i<26; i++){
+        if(root->child[i]!=NULL) {
+            word[level] = i + 'a';
+            display(root->child[i], word, level+1);
+        }
+    }
+}
+
+// populating bloom filter and trie with Dictionary words
+void loadDictionary(bool* filter, TRIE_NODE* root){
+    FILE *file = fopen("dictionary.txt", "r");
+    char word[50];
+    while(fscanf(file, "%s", word) != EOF) {
+        insertFilter(filter, word);
+        insertTrie(root, word);
+    }
     fclose(file);
+}
+
+int levenshteinDistance(char* s1, char* s2){    // algorithm used to find similar correct words
+    int n = strlen(s1), m = strlen(s2);
+    if(!(n & m)) return 0;
+    int cost = 0;
+    int dp[MAX_LENGTH + 1][MAX_LENGTH + 1];
+    for(int i=0; i<=n; i++) dp[i][0] = i;
+    for(int j=0; j<=m; j++) dp[0][j] = j;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            cost = (s1[i-1] == s2[j-1]) ? 0 : 1;
+            dp[i][j] = smallest(dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + cost);
+        }
+    }
+    return dp[n][m];
 }
 
 int main() {
     // initializing bloom filter
-    filter = calloc(filterSize, sizeof(bool));
+    printf("Loading dictionary...");
+    bool* filter = calloc(FILTER_SIZE, sizeof(bool));
+    TRIE_NODE* root = createNode();
 
-    // populating bloom filter with dictionary
-    loadDictionary();
+    // populating bloom filter and trie with dictionary
+    loadDictionary(filter, root);
+    printf("Loaded Dictionary on Bloom Filter and Trie.");
 
     // testing bloom filter
     while(1) {
@@ -60,7 +139,11 @@ int main() {
         char input[50];
         fscanf(stdin, "%s", input);
         for(int i = 0; input[i]; i++) input[i] = tolower(input[i]);
-        if(search(input)) printf("\nword present"); // search
+        printf("\nUsing Bloom Filter:");
+        if(searchFilter(filter, input)) printf("\nword present"); // search
+        else printf("\nword not present");
+        printf("\nUsing Trie:");
+        if(searchTrie(root, input)) printf("\nword present"); // search
         else printf("\nword not present");
     }
     
